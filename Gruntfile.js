393be10d8e58b1128fe70fbd2ba4c0ac0523423a
Zordius Chen <zordius@yahoo-inc.com>
2014-08-27 16:21:01 +0800

    fix typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
             {browserName: 'safari', version: 6, platform: 'OS X 10.8'},
             {browserName: 'internet explorer', version: 11, platform: 'Windows 8.1'},
             {browserName: 'internet explorer', version: 10, platform: 'Windows 8'},
-            {browserName: 'internet explorer', version: 9, platform: 'Windows 7'}
+            {browserName: 'internet explorer', version: 9, platform: 'Windows 7'},
             {browserName: 'iphone', version:'7.1', platform: 'OS X 10.9'},
             {browserName: 'iphone', version:'7.0', platform: 'OS X 10.9'},
             {browserName: 'iphone', version:'6.1', platform: 'OS X 10.8'},
