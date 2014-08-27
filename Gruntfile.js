25215fbae21068a2e1107643aa592e7ccb0289ed
Zordius Chen <zordius@yahoo-inc.com>
2014-08-27 16:17:17 +0800

    try ie9

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,11 +29,11 @@ module.exports = function (grunt) {
             {browserName: 'safari', version: 6, platform: 'OS X 10.8'},
             {browserName: 'internet explorer', version: 11, platform: 'Windows 8.1'},
             {browserName: 'internet explorer', version: 10, platform: 'Windows 8'},
-//            {browserName: 'internet explorer', version: 9, platform: 'Windows 7'}
-            {browserName: 'iphone'},
+            {browserName: 'internet explorer', version: 9, platform: 'Windows 7'}
+            {browserName: 'iphone', version:'7.1', platform: 'OS X 10.9'},
             {browserName: 'iphone', version:'7.0', platform: 'OS X 10.9'},
             {browserName: 'iphone', version:'6.1', platform: 'OS X 10.8'},
-            {browserName: 'ipad'},
+            {browserName: 'ipad', version:'7.1', platform: 'OS X 10.9'},
             {browserName: 'ipad', version:'7.0', platform: 'OS X 10.9'},
             {browserName: 'ipad', version:'6.1', platform: 'OS X 10.8'}
           ]
