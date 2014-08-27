e68dfdb46fdcc7b0efa52a87a85816a662a0767b
Zordius Chen <zordius@yahoo-inc.com>
2014-08-27 10:45:39 +0800

    prevent travis ci error. run grunt in job 2 ( nodejs 10)

--- travis.sh
+++ travis.sh
@@ -2,12 +2,14 @@
 
 echo "DEBUG ENV: ${TRAVIS_JOB_NUMBER} ${TRAVIS_BUILD_NUMBER} ..."
 
-if [ "${TRAVIS_BUILD_NUMBER}.1" != "${TRAVIS_JOB_NUMBER}" ]; then
+if [ "${TRAVIS_BUILD_NUMBER}.2" != "${TRAVIS_JOB_NUMBER}" ]; then
   echo "Only run sauce labs 1 time... quit."
   exit 0
 fi
 
 # build JS files for dist and test
+npm install grunt grunt-cli grunt-contrib-connect grunt-saucelabs
+
 npm run-script build_std
 npm run-script build_dbg
 npm run-script build_min
