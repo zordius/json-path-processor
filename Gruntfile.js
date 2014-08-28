8fd04794e18b190702a7eb3830b2d595d1246669
Zordius Chen <zordius@yahoo-inc.com>
2014-08-28 17:56:52 +0800

    this cause error.....test.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,7 @@ module.exports = function (grunt) {
           concurrency: 2,
           'max-duration': 60,
           browsers: [
+            {browserName: 'opera'},
             {browserName: 'chrome'},
             {browserName: 'firefox'},
             {browserName: 'safari', version: 7, platform: 'OS X 10.9'},
