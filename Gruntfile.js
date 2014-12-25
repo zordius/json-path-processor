1ae32807fe343ec0f8da321e0d5f33d92433fc23
Zordius Chen <zordius@yahoo-inc.com>
2014-12-25 09:08:31 +0800

    add polyfill and test on ie 7/8

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,8 @@ module.exports = function (grunt) {
             {browserName: 'internet explorer', version: 11, platform: 'Windows 8.1'},
             {browserName: 'internet explorer', version: 10, platform: 'Windows 8'},
             {browserName: 'internet explorer', version: 9, platform: 'Windows 7'},
+            {browserName: 'internet explorer', version: 8, platform: 'Windows 7'},
+            {browserName: 'internet explorer', version: 7, platform: 'Windows XP'},
             {browserName: 'iphone', version:'7.1', platform: 'OS X 10.9'},
             {browserName: 'iphone', version:'7.0', platform: 'OS X 10.9'},
             {browserName: 'iphone', version:'6.1', platform: 'OS X 10.8'},
