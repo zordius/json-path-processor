fdd467b6c3f39fb37b6dfa464fbe9d2b2017b57b
Zordius Chen <zordius@yahoo-inc.com>
2015-06-23 17:51:15 +0800

    test on node10

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 
 echo "DEBUG ENV: ${TRAVIS_JOB_NUMBER} ${TRAVIS_BUILD_NUMBER} ..."
 
-if [ "${TRAVIS_BUILD_NUMBER}.2" != "${TRAVIS_JOB_NUMBER}" ]; then
+if [ "${TRAVIS_BUILD_NUMBER}.1" != "${TRAVIS_JOB_NUMBER}" ]; then
   echo "Only run sauce labs 1 time 1 commit... quit."
   exit 0
 fi
