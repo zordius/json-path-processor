77797e52668c55cad2737b5aec2686c7060ef8d4
Zordius Chen <1201409+zordius@users.noreply.github.com>
2017-09-15 16:24:17 +0800

    try this to debug sc

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,10 @@ module.exports = cfg => {
       public: 'public',
       tags: [process.env.TRAVIS_JOB_ID, process.env.TRAVIS_COMMIT, 'jpp', 'mocha', 'karma']
     },
-    logLevel: cfg.LOG_DEBUG,
+    connectOptions: {
+      verbose: true,
+      doctor: true
+    },
     customLaunchers,
     browsers: Object.keys(customLaunchers),
     frameworks: ['mocha', 'browserify'],
